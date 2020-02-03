4ae59c7cc8bcf737092a8eb5fcf22eed3220b0ab
Ann <lilpowerfulann@gmail.com>
2020-02-03 15:43:26 +0300

    classes creation

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,9 @@
-public class Animal {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Animal {
+    List<Food> food = new ArrayList<>();
+    String type;
+    String name;
+    int age;
 }
