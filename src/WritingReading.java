e79f8dc459bf8d2586368f06c9bf4354d2376251
Ann <lilpowerfulann@gmail.com>
2020-02-03 16:34:26 +0300

    classes update

--- src/WritingReading.java
+++ src/WritingReading.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -6,9 +8,18 @@ public class WritingReading {
     public static void writing(Animal animal, String file ) {
         try (FileWriter writer = new FileWriter(file, false)) {
             writer.write(animal.toString());
-
         } catch (IOException e) {
             e.printStackTrace();
         };
+    }
+
+    public static void reading(String file) {
+        try (FileReader reader = new FileReader(file)) {
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
